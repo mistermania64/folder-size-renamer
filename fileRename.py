3edca26dd1b0e63f010a5f55d9b9c18e4e00f974
mistermania64 <81997874+mistermania64@users.noreply.github.com>
2026-03-25 23:41:20 -0400

    update try/except on nested function call
    update try/except on nested function call to ensure script doesn't get stuck on renaming folders

--- fileRename.py
+++ fileRename.py
@@ -32,53 +32,61 @@ def get_dir_size(path='.'):
     return total_size
 
 for filename in files:
-    if os.path.isdir(filename):
-        print("Folder: " + filename + ".\n")
-        size = get_dir_size(filename)
+    # Skip hidden folders
+    if not filename.startswith("$") and not filename.startswith("."):
 
-        # 1 GB = 1073741824 Bytes
-        # 1 MB = 1048576 Bytes
-        # 1 KB = 1024 Bytes
+        if os.path.isdir(filename):
+
+            print("Folder: " + filename + ".\n")
+
+            try:
+                 size = get_dir_size(filename)
+            except (PermissionError, FileNotFoundError):
+                continue
+
+            # 1 GB = 1073741824 Bytes
+            # 1 MB = 1048576 Bytes
+            # 1 KB = 1024 Bytes
+            
+            # If size is less than one kilobyte, display size in bytes (B)
+            if size < 1024:
+                new_size = size # No conversion necessary
+                size_type = " B"
+
+            # If size is less than one megabyte, display size in kilobytes (KB)
+            elif size < 1048576: 
+                new_size = (size / (1024)) # Convert size to kilobytes
+                size_type = " KB"
+
+            # If size is less than one gigabyte, display size in megabytes (MB)
+            elif size < 1073741824:
+                new_size = (size / (1024**2))  # Convert size to megabytes
+                size_type = " MB"      
+
+            # Else, size is at least one gigabyte, display in gigabytes (GB)
+            else: # size >= 1073741824: 
+                new_size = (size / (1024**3)) # Convert size to gigabytes
+                size_type = " GB"
         
-        # If size is less than one kilobyte, display size in bytes (B)
-        if size < 1024:
-            new_size = size # No conversion necessary
-            size_type = " B"
+            # Skip hidden folders
+            if not filename.startswith("$") and not filename.startswith("."):
 
-        # If size is less than one megabyte, display size in kilobytes (KB)
-        elif size < 1048576: 
-            new_size = (size / (1024)) # Convert size to kilobytes
-            size_type = " KB"
+                # For folders that have already been calculated, replace size instead of appending
+                if (filename.endswith("B]")):
 
-        # If size is less than one gigabyte, display size in megabytes (MB)
-        elif size < 1073741824:
-            new_size = (size / (1024**2))  # Convert size to megabytes
-            size_type = " MB"      
+                    # Match size pattern in string
+                    old_size = r'\[\d+\.\d+\s[K,M,G]?B\]' 
+                    
+                    # Construct filename with new size
+                    new_size_string = "[" + "{:.1f}".format(new_size) + size_type + "]"
 
-        # Else, size is at least one gigabyte, display in gigabytes (GB)
-        else: # size >= 1073741824: 
-            new_size = (size / (1024**3)) # Convert size to gigabytes
-            size_type = " GB"
-        
-        # Skip hidden folders
-        if not filename.startswith("$") and not filename.startswith("."):
+                    # Create string to overwrite old filename
+                    renamed_string = re.sub(old_size, new_size_string, filename, count=1) 
+            
+                    os.rename(filename, renamed_string)
 
-            # For folders that have already been calculated, replace size instead of appending
-            if (filename.endswith("B]")):
-
-                # Match size pattern in string
-                old_size = r'\[\d+\.\d+\s[K,M,G]?B\]' 
-                
-                # Construct filename with new size
-                new_size_string = "[" + "{:.1f}".format(new_size) + size_type + "]"
-
-                # Create string to overwrite old filename
-                renamed_string = re.sub(old_size, new_size_string, filename, count=1) 
-        
-                os.rename(filename, renamed_string)
-
-            else:
-                os.rename(str(filename), str(filename) + " [" + "{:.1f}".format(new_size) + size_type + "]")
-                
+                else:
+                    os.rename(str(filename), str(filename) + " [" + "{:.1f}".format(new_size) + size_type + "]")
+                    
     else:
         print("File: " + filename + ".\n")
--- fileRenameStrip.py
+++ fileRenameStrip.py
@@ -5,6 +5,7 @@
 import os
 import re
 
+# Global Variables
 path = os.getcwd()
 files = os.listdir(path)
 
@@ -32,45 +33,50 @@ def get_dir_size(path='.'):
     return total_size
 
 for filename in files:
-    if os.path.isdir(filename):
-        print("Folder: " + filename + ".\n")
-        size = get_dir_size(filename)
-
-        # 1 GB = 1073741824 Bytes
-        # 1 MB = 1048576 Bytes
-        # 1 KB = 1024 Bytes
+    # Skip hidden folders
+    if not filename.startswith("$") and not filename.startswith("."):
         
-        # If size is less than one kilobyte, display size in bytes (B)
-        if size < 1024:
-            new_size = size # No conversion necessary
-            size_type = " B"
+        if os.path.isdir(filename):
 
-        # If size is less than one megabyte, display size in kilobytes (KB)
-        elif size < 1048576: 
-            new_size = (size / (1024)) # Convert size to kilobytes
-            size_type = " KB"
+            print("Folder: " + filename + ".\n")
 
-        # If size is less than one gigabyte, display size in megabytes (MB)
-        elif size < 1073741824:
-            new_size = (size / (1024**2))  # Convert size to megabytes
-            size_type = " MB"      
+            try:
+                 size = get_dir_size(filename)
+            except (PermissionError, FileNotFoundError):
+                continue
 
-        # Else, size is at least one gigabyte, display in gigabytes (GB)
-        else: # size >= 1073741824: 
-            new_size = (size / (1024**3)) # Convert size to gigabytes
-            size_type = " GB"
-        
-        # Skip hidden folders
-        if not filename.startswith("$") and not filename.startswith("."):
+            # 1 GB = 1073741824 Bytes
+            # 1 MB = 1048576 Bytes
+            # 1 KB = 1024 Bytes
+            
+            # If size is less than one kilobyte, display size in bytes (B)
+            if size < 1024:
+                new_size = size # No conversion necessary
+                size_type = " B"
+
+            # If size is less than one megabyte, display size in kilobytes (KB)
+            elif size < 1048576: 
+                new_size = (size / (1024)) # Convert size to kilobytes
+                size_type = " KB"
+
+            # If size is less than one gigabyte, display size in megabytes (MB)
+            elif size < 1073741824:
+                new_size = (size / (1024**2))  # Convert size to megabytes
+                size_type = " MB"      
+
+            # Else, size is at least one gigabyte, display in gigabytes (GB)
+            else: # size >= 1073741824: 
+                new_size = (size / (1024**3)) # Convert size to gigabytes
+                size_type = " GB"
 
             # For folders that have already been calculated, replace size instead of appending
-            if (filename.endswith("B]")):
+            if (filename.endswith("]")):
 
                 # Match size pattern in string
                 old_size = r'\[\d+\.\d+\s[K,M,G]?B\]' 
                 
                 # Construct filename with new size
-                new_size_string = "[" + "{:.1f}".format(new_size) + size_type + "]"
+                # new_size_string = "[" + "{:.1f}".format(new_size) + size_type + "]"
                 empty = ""
 
                 # Create string to overwrite old filename
